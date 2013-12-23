db91f823c847db8471fe1546abe9485065c0c968
karavanjo <jownkiv@gmail.com>
2013-12-23 03:36:37 +0300

    functionality for GeoJson and WMS layers, for creating markers was implemented

--- rosavto/static/js/rosavto/Map.js
+++ rosavto/static/js/rosavto/Map.js
@@ -1,24 +1,87 @@
 define([
     'dojo/_base/declare',
-    'dijit/_WidgetBase',
-    'http://cdn.leafletjs.com/leaflet-0.7.1/leaflet.js?2'
-], function (declare, _WidgetBase) {
+    'dojo/_base/lang',
+    'dojo/request/xhr',
+    'http://cdn.leafletjs.com/leaflet-0.7.1/leaflet-src.js'
+], function (declare, lang, xhr) {
     return declare('Map', [], {
+        _map: {},
+        _baseLayers: {},
+        _overlaylayers: {},
+        _legend: null,
+
         constructor: function (domNode, settings) {
+            if (!settings.zoomControl) {
+                settings.zoomControl = false;
+            }
+
             this._map = new L.Map(domNode, settings);
+
+            if (settings.legend) {
+                this._legend = L.control.layers(this._baseLayers, this._overlaylayers).addTo(this._map);
+            }
+
             this.addOsmTileLayer();
         },
 
-        addTileLayer: function (url, settings) {
-            this._map.addLayer(new L.TileLayer(url, settings));
+        addWmsLayer: function (url, name, settings) {
+            var wmsLayer = L.tileLayer.wms(url, settings);
+
+            this._baseLayers[name] = wmsLayer;
+            if (this._legend) this._legend.addBaseLayer(wmsLayer, name);
+        },
+
+        addTileLayer: function (name, url, settings) {
+            var tileLayer = new L.TileLayer(url, settings);
+            this._map.addLayer(tileLayer);
+            this._baseLayers[name] = tileLayer;
+            if (this._legend) this._legend.addBaseLayer(tileLayer, name);
         },
 
         addOsmTileLayer: function () {
-            var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                 settingsOsmLayer = {
                     attribution: 'Map data © OpenStreetMap contributors'
                 };
-            this.addTileLayer(osmUrl, settingsOsmLayer);
+
+            this.addTileLayer('Openstreetmap', osmUrl, settingsOsmLayer);
+        },
+
+        addGeoJsonLayer: function (name, url, style) {
+            xhr(application_root + url, {
+                handleAs: 'json'
+            }).then(lang.hitch(this, function (geoJson) {
+                    if (geoJson.features) {
+                        var layer = L.geoJson(geoJson.features, {
+                            style: style,
+                            pointToLayer: function (feature, latlng) {
+                                return L.circle(latlng, 25, style);
+                            }
+                        });
+
+                        layer.addTo(this._map);
+
+                        this._overlaylayers[name] = layer;
+                        if (this._legend) this._legend.addOverlay(layer, name);
+                    }
+                }));
+        },
+
+        showObjectAsMarker: function (url, id, isPopup) {
+            xhr(application_root + url + id, {
+                handleAs: 'json'
+            }).then(lang.hitch(this, function (geoJson) {
+                    if (geoJson.features) {
+                        var layer = new L.geoJson(geoJson.features, {
+                            onEachFeature: function (feature, layer) {
+                                if (isPopup) {
+                                    layer.bindPopup('id: ' + feature.id);
+                                }
+                            }
+                        });
+                        this._map.addLayer(layer);
+                    }
+                }));
         }
     });
 });
