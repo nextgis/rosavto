db586c132fcc131f07e53b6d1ea82f033335b771
karavanjo <jownkiv@gmail.com>
2014-01-14 08:27:07 +0300

    Attributes route and proxy for LayersInfo was added

--- rosavto/views/__init__.py
+++ rosavto/views/__init__.py
@@ -1,4 +1,6 @@
 from pyramid.view import view_config
+import urllib2
+import ast
 
 
 @view_config(route_name='map', renderer='map.mako')
@@ -24,3 +26,16 @@ def wms(request):
 @view_config(route_name='realtime', renderer='realtime.mako')
 def realtime(request):
     return {}
+
+
+@view_config(route_name='attributes', renderer='attributes.mako')
+def attributes(request):
+    return {}
+
+
+@view_config(route_name='getLayersInfo', renderer='json')
+def get_layers_info(request):
+    result = None
+    if 'url' in request.POST:
+        result = ast.literal_eval(urllib2.urlopen(request.POST['url']).read())
+    return result
