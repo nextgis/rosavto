49400cb85fde1ac1abb9c6e1fef332ea39ea7d3f
karavanjo <jownkiv@gmail.com>
2014-01-19 18:25:46 +0300

    support of popup building for MapIdentity was implemented

--- rosavto/static/js/rosavto/MapIdentify.js
+++ rosavto/static/js/rosavto/MapIdentify.js
@@ -3,22 +3,27 @@ define([
     'dojo/_base/array',
     'dojo/_base/lang',
     'dojo/request/xhr',
-    'rosavto/LayersInfo',
-    'proj4js/proj4js'
+    'dojo/topic',
+    'dijit/_WidgetBase',
+    'dijit/_TemplatedMixin',
+    'dijit/_WidgetsInTemplateMixin',
+    'dojo/text!./templates/MapIdentify.html',
+    'mustache/mustache'
 ],
-    function (declare, array, lang, xhr, LayersInfo, Proj4js) {
-        return declare('rosavto.MapIdentify', null, {
+    function (declare, array, lang, xhr, topic, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template, mustache) {
+
+        return declare('rosavto.MapIdentify', [], {
+            template: template,
 
             constructor: function (map, layersInfo, settings) {
+                mustache.parse(this.template);
                 this._map = map;
                 this._layersInfo = layersInfo;
                 lang.mixin(this, settings);
-                this.defineCrs();
             },
 
-            defineCrs: function () {
-                Proj4js.defs["WGS84"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
-                Proj4js.defs["EPSG:3857"] = "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
+            _stringRepl: function (tmpl) {
+                return mustache.render(tmpl, this);
             },
 
             on: function () {
@@ -38,24 +43,25 @@ define([
             },
 
             getIdsByClick: function (e) {
-                var map = this._map._lmap;
+                var map = this._map._lmap,
+                    latlngClick = e.latlng;
 
-                this._layersInfo.getLayersIdByStyles(this._map._ngwTileLayers).then(lang.hitch(this, function (layersId) {
-                    var source = new Proj4js.Proj('WGS84'),
-                        dest = new Proj4js.Proj('EPSG:3857'),
-                        point = map.project([e.latlng.lat, e.latlng.lng], map.getZoom()),
-                        pointTopLeft = map.unproject(new L.Point(point.x - 10, point.y - 10), map.getZoom()),
-                        pointBottomRight = map.unproject(new L.Point(point.x + 10, point.y + 10), map.getZoom()),
-                        wktBounds, postParams, xhrIdentity;
+                return this._layersInfo.getLayersIdByStyles(this._map._ngwTileLayers).then(lang.hitch(this, function (layersId) {
+                    var point = map.project([e.latlng.lat, e.latlng.lng], map.getZoom()),
+                        pointTopLeft = L.CRS.EPSG3857.project(map.unproject(new L.Point(point.x - 10, point.y - 10), map.getZoom())),
+                        pointBottomRight = L.CRS.EPSG3857.project(map.unproject(new L.Point(point.x + 10, point.y + 10), map.getZoom())),
+                        wktBounds,
+                        postParams,
+                        xhrIdentity;
 
-                    wktBounds = 'POLYGON((' + pointTopLeft.lng + ' ' + pointTopLeft.lat + ', ' +
-                        pointBottomRight.lng + ' ' + pointTopLeft.lat + ', ' +
-                        pointBottomRight.lng + ' ' + pointBottomRight.lat + ', ' +
-                        pointTopLeft.lng + ' ' + pointBottomRight.lat + ', ' +
-                        pointTopLeft.lng + ' ' + pointTopLeft.lat + '))';
+                    wktBounds = 'POLYGON((' + pointTopLeft.x + ' ' + pointTopLeft.y + ', ' +
+                        pointBottomRight.x + ' ' + pointTopLeft.y + ', ' +
+                        pointBottomRight.x + ' ' + pointBottomRight.y + ', ' +
+                        pointTopLeft.x + ' ' + pointBottomRight.y + ', ' +
+                        pointTopLeft.x + ' ' + pointTopLeft.y + '))';
 
                     postParams = {
-                        src: 4326,
+                        srs: 3857,
                         geom: wktBounds,
                         layers: layersId
                     };
@@ -63,9 +69,70 @@ define([
                     xhrIdentity = this.proxy ? xhr(this.proxy, {handleAs: 'json', method: 'POST', data: {url: this.url, params: JSON.stringify(postParams)}}) :
                         xhr(this.url, {handleAs: 'json', method: 'POST', data: postParams, headers: {'X-Requested-With': 'XMLHttpRequest'}});
 
-                    xhrIdentity.then(function (data) {
-                        alert(data);
-                    });
+                    xhrIdentity.then(lang.hitch(this, function (featuresInfo) {
+                        var layerName,
+                            layerId,
+                            featureCount,
+                            feature,
+                            i,
+                            featuresIdentified = {
+                                count: 0,
+                                features: {}
+                            };
+
+                        for (layerId in featuresInfo) {
+                            if (featuresInfo.hasOwnProperty(layerId)) {
+                                featureCount = featuresInfo[layerId]['featureCount'];
+                                if (featureCount > 0) {
+                                    featuresIdentified.count += featureCount;
+                                    layerName = this._layersInfo.getLayerNameByLayerId(parseInt(layerId, 10));
+                                    featuresIdentified.features[layerName] = [];
+                                    for (i = 0; i < featureCount; i += 1) {
+                                        feature = featuresInfo[layerId].features[i];
+                                        featuresIdentified.features[layerName].push({
+                                            label: feature.label,
+                                            id: feature.fields[this.fieldIdentify]
+                                        });
+                                    }
+                                }
+                            }
+                        }
+
+                        if (featuresIdentified.count < 2) {
+                            topic.publish('map/identity', featuresIdentified);
+                        } else {
+                            var layersTemplated = [],
+                                layerTemplated;
+                            for (layerName in featuresIdentified.features) {
+                                if (featuresIdentified.features.hasOwnProperty(layerName)) {
+                                    feturesCount = featuresIdentified.features[layerName].length;
+
+                                    if (featureCount < 1) {
+                                        continue;
+                                    }
+
+                                    layerTemplated = {
+                                        'name': layerName,
+                                        'features': []
+                                    };
+
+                                    for (i = 0; i < feturesCount; i++) {
+                                        feature = featuresIdentified.features[layerName][i];
+                                        layerTemplated.features.push({
+                                            id: feature.id,
+                                            name: feature.label
+                                        });
+                                    }
+
+                                    layersTemplated.push(layerTemplated);
+                                }
+                            }
+
+                            L.popup().setLatLng(latlngClick)
+                                .setContent(mustache.render(this.template, {layers: layersTemplated}))
+                                .openOn(map);
+                        }
+                    }));
                 }));
             }
         });
