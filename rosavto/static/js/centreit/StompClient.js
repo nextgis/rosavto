528436909bc01f4aebf0b31aedd6ccaad12c3ca9
karavanjo <jownkiv@gmail.com>
2014-03-24 13:12:08 +0300

    styling code

--- rosavto/static/js/centreit/StompClient.js
+++ rosavto/static/js/centreit/StompClient.js
@@ -14,77 +14,81 @@
  * Для кода на ExtJS экземпляр этого класса помещён в Monitoring.stomp.
  *
  */
-define(['dojo/topic', 'dojo/_base/declare', 'when', 'dojo/_base/lang', 'dojox/lang/functional/object','Stomp', 'SockJS'], function (topic, declare, when, lang, object) {
-    return {
-        topic: topic,
-        reconnectDelay: 1000,
-        debug: false, // заменить на true для включения отладочных сообщений Stomp.js и Sock.js
-        deferred: null,
-        client: null,
-        connect: function () {
-            if (!this.deferred) {
-                this.deferred = when.defer();
-                this.reconnect();
-            }
-            return this.deferred.promise;
-        },
-
-        reconnect: function() {
-            var client = Stomp.over(new SockJS(Monitoring.contextPath+'socket', null, {debug: this.debug}));
-            if (!this.debug) {
-                client.debug = null;
-            }
-            client.connect('spring', 'spring', lang.hitch(this, function () {
-                this.internalClient.resubscribe(client);
-                this.deferred.resolve(this.internalClient);
-                topic.publish("connection/status", {
-                    closed: false
-                });
-            }), lang.hitch(this, function(error) {
-                console.warn('Подключение по websocket не удалось: ' + error + '. Следующая попытка через ' + this.reconnectDelay + ' мс.');
-                topic.publish("connection/status", {
-                    closed: true
-                });
-                setTimeout(lang.hitch(this, this.reconnect), this.reconnectDelay);
-            }));
-        },
-
-        internalClient: {
+define([
+    'dojo/topic', 'dojo/_base/declare', 'when',
+    'dojo/_base/lang', 'dojox/lang/functional/object',
+    'Stomp', 'SockJS'],
+    function (topic, declare, when, lang, object) {
+        return {
+            topic: topic,
+            reconnectDelay: 1000,
+            debug: false, // заменить на true для включения отладочных сообщений Stomp.js и Sock.js
+            deferred: null,
             client: null,
-            subscriptions: [],
-            subscribe: function(destination, callback, headers) {
-                var result = this.client.subscribe(destination, callback, headers);
-                this.subscriptions[result.id] = {destination: destination, callback: callback, headers: headers, result: result};
-                result.unsubscribe = lang.hitch(this, lang.partial(this.unsubscribe, result.id));
-                return result;
-            },
-            unsubscribe: function(id) {
-                delete this.subscriptions[id];
-                return this.client.unsubscribe(id);
-            },
-            resubscribe: function(client) {
-                var me = this;
-                if (me.client) {
-                    object.forIn(this.subscriptions, function(subscription, id){
-                        me.client.unsubscribe(id);
-                    });
-                    me.client.disconnect();
+            connect: function () {
+                if (!this.deferred) {
+                    this.deferred = when.defer();
+                    this.reconnect();
                 }
-                me.client = client;
-                object.forIn(this.subscriptions, function(subscription, id){
-                    if (subscription.headers) {
-                        delete subscription.headers.id;
+                return this.deferred.promise;
+            },
+
+            reconnect: function () {
+                var client = Stomp.over(new SockJS(Monitoring.contextPath + 'socket', null, {debug: this.debug}));
+                if (!this.debug) {
+                    client.debug = null;
+                }
+                client.connect('spring', 'spring', lang.hitch(this, function () {
+                    this.internalClient.resubscribe(client);
+                    this.deferred.resolve(this.internalClient);
+                    topic.publish("connection/status", {
+                        closed: false
+                    });
+                }), lang.hitch(this, function (error) {
+                    console.warn('Подключение по websocket не удалось: ' + error + '. Следующая попытка через ' + this.reconnectDelay + ' мс.');
+                    topic.publish("connection/status", {
+                        closed: true
+                    });
+                    setTimeout(lang.hitch(this, this.reconnect), this.reconnectDelay);
+                }));
+            },
+
+            internalClient: {
+                client: null,
+                subscriptions: [],
+                subscribe: function (destination, callback, headers) {
+                    var result = this.client.subscribe(destination, callback, headers);
+                    this.subscriptions[result.id] = {destination: destination, callback: callback, headers: headers, result: result};
+                    result.unsubscribe = lang.hitch(this, lang.partial(this.unsubscribe, result.id));
+                    return result;
+                },
+                unsubscribe: function (id) {
+                    delete this.subscriptions[id];
+                    return this.client.unsubscribe(id);
+                },
+                resubscribe: function (client) {
+                    var me = this;
+                    if (me.client) {
+                        object.forIn(this.subscriptions, function (subscription, id) {
+                            me.client.unsubscribe(id);
+                        });
+                        me.client.disconnect();
                     }
-                    var newResult = this.client.subscribe(subscription.destination, subscription.callback,
-                        subscription.headers === void(0) ? null : subscription.headers);
-                    subscription.result.id = newResult.id;
-                    subscription.result.unsubscribe = lang.hitch(me, lang.partial(me.unsubscribe, newResult.id));
-                });
+                    me.client = client;
+                    object.forIn(this.subscriptions, function (subscription, id) {
+                        if (subscription.headers) {
+                            delete subscription.headers.id;
+                        }
+                        var newResult = this.client.subscribe(subscription.destination, subscription.callback,
+                            subscription.headers === void(0) ? null : subscription.headers);
+                        subscription.result.id = newResult.id;
+                        subscription.result.unsubscribe = lang.hitch(me, lang.partial(me.unsubscribe, newResult.id));
+                    });
+                }
+
             }
 
-        }
-
-    };
+        };
 
 
-});
+    });
