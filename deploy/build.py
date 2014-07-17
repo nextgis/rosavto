c0ce8e5abe6f28fe5f2d6c8c8077d0c91027a973
karavanjo <jownkiv@gmail.com>
2014-07-18 02:53:23 +0300

    script for building ngw_rosavto app was implemented

--- deploy/build.py
+++ deploy/build.py
@@ -19,12 +19,13 @@ print 'building release {0}'.format(release_number)
 
 release_version_regex = ur'V\d.\d.\d'
 
-link_to_rosavto = '/home/cloud/projects/widgets/rosavto'
 path_to_release_folder = '/home/cloud/projects/gis/V{0}/'.format(release_number)
 
 if not path.exists(path_to_release_folder):
     raise Exception('path "{0}" to release folder not found'.format(path_to_release_folder))
 
+# build of rosavto app
+link_to_rosavto = '/home/cloud/projects/widgets/rosavto'
 if not path.exists(link_to_rosavto):
     raise Exception('path "{0}" to ROSAVTO symbolic link folder not found'.format(link_to_rosavto))
 
@@ -32,8 +33,8 @@ if path.islink(link_to_rosavto):
     path_to_rosavto = path.realpath(link_to_rosavto)
     current_release_version = re.findall(release_version_regex, path_to_rosavto)
     if len(current_release_version) == 1:
-        if current_release_version[0][1:] == args['release']:
-            print 'release {0} already assembled'.format(args['release'])
+        if current_release_version[0][1:] == release_number:
+            print 'release {0} already assembled'.format(release_number)
         else:
             print 'building release {0} started...'.format(release_number)
             new_path = '/home/cloud/projects/gis/V{0}/widgets/'.format(release_number)
@@ -43,4 +44,26 @@ if path.islink(link_to_rosavto):
             old_link = os.readlink(link_to_rosavto)
             os.unlink(link_to_rosavto)
             os.symlink(new_path, link_to_rosavto)
+            print 'building release {0} has finished.'.format(release_number)
+
+# build of ngw_rosavto app
+link_to_ngw_rosavto = '/home/cloud/projects/ngw/nextgisweb_rosavto'
+if not path.exists(link_to_ngw_rosavto):
+    raise Exception('path "{0}" to NEXTGISWEB_ROSAVTO symbolic link folder not found'.format(link_to_ngw_rosavto))
+
+if path.islink(link_to_ngw_rosavto):
+    path_to_ngw_rosavto = path.realpath(link_to_ngw_rosavto)
+    current_release_version = re.findall(release_version_regex, path_to_ngw_rosavto)
+    if len(current_release_version) == 1:
+        if current_release_version[0][1:] == release_number:
+            print 'release {0} already assembled'.format(release_number)
+        else:
+            print 'building release {0} started...'.format(release_number)
+            new_path = '/home/cloud/projects/gis/V{0}/nextgisweb_rosavto/'.format(release_number)
+            print 'install rosavto V{0}...'.format(release_number)
+            subprocess.call('cd /home/cloud/projects/ngw; /home/cloud/projects/env/bin/pip install -e nextgisweb_rosavto;'.format(new_path), shell=True)
+            print 'replace symlink...'
+            old_link = os.readlink(link_to_ngw_rosavto)
+            os.unlink(link_to_ngw_rosavto)
+            os.symlink(new_path, link_to_ngw_rosavto)
             print 'building release {0} has finished.'.format(release_number)
