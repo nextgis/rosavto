b7374c00f6bc79fc3e08a2d2a00707eee320e27f
karavanjo <jownkiv@gmail.com>
2015-05-08 11:57:27 +0300

    get_page_config function for getting page_config byr route_name was implemented

--- rosavto/navigation.py
+++ rosavto/navigation.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
 
-pages = [
-    ('index', u'Главная', u'На главную'),
-    ('', u'Слои', u'Подключение слоев', [
-        ('wms', u'WMS', u'WMS слой'),
-        ('base_layers', u'Подложки', u'Подложки из NextGIS Web'),
-        ('layers', u'Тайловые слои', u'Тайловые слои из NextGIS Web')
+pages_config = [
+    ('index', u'Главная', u'На главную', u''),
+    (None, u'Слои', u'Подключение слоев', None, [
+        ('wms', u'WMS', u'WMS слой', u'Подключение WMS слоя'),
+        ('base_layers', u'Подложки', u'Подключение подложек из NextGIS Web', u'Подключение подложек из NextGIS Web'),
+        ('layers', u'Тайловые слои', u'Подключение тайловых слоев из NextGIS Web',
+         u'Подключение тайловых слоев из NextGIS Web')
     ]),
 ]
+
+
+def get_page_config(route_name):
+    page_config = search_page_config(pages_config, route_name)
+    return {
+        'title': page_config[3]
+    }
+
+
+def search_page_config(config, route_name):
+    for config_item in config:
+        if len(config_item) == 4:
+            route_name_current, menu_item_title, menu_item_descr, header_text = config_item
+            if route_name_current == route_name:
+                return config_item
+        else:
+            if config_item[0] is None:
+                result = search_page_config(config_item[4], route_name)
+                if result:
+                    return result
