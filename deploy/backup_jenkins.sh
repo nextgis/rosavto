c748c6b1ecd82df617a80e9fe8a2a06030d61e2c
Ivan Kavaliou <jownkiv@gmail.com>
2016-02-01 16:14:44 +0300

    #31 backup script was implemented

--- deploy/backup_jenkins.sh
+++ deploy/backup_jenkins.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
+
+set -e
+
 backup () {
-    mkdir -p ~/mnt/V$1
+    backup_dir="$HOME/mnt/V$1"
+    time_label=$(date -u +%d.%m.%Y_%H.%M.%S)
+    temporary_dir="$HOME/temp/$time_label"
+    zip_file_name="$HOME/temp/V$1-$2-$time_label.tar.gz"
 
-    BACKUP_FILE_NAME=rosavto-$2.gis'('$1')'$(date -d "$xx -1 day" -u +%d.%m.%Y-%H.%M.%S).backup
-    rm -f ~/mnt/V$1/$BACKUP_FILE_NAME
+    echo Creating backup_dir - "$backup_dir"
+    mkdir -p "$backup_dir"
+    echo backup_dir was created
 
-    pg_dumpall -c -O > ~/mnt/V$1/$BACKUP_FILE_NAME
-    if ! [ -s ~/mnt/V$1/$BACKUP_FILE_NAME ] ; then
-        rm -f ~/mnt/V$1/$BACKUP_FILE_NAME
-        echo created backup file was removed
-        exit 1
-    else
-            echo ------------
-            echo SERVER: gis-$2 [192.168.255.101]
-            echo VERSION_BUID_GIS: $1
-            echo BACKUP_FILE_NAME: $BACKUP_FILE_NAME
-            echo ------------
-    fi ;
+    echo Creating temporary_dir - "$temporary_dir"
+    mkdir -p "$temporary_dir"
+    echo temporary_dir was created
+
+#    echo Dumping database...
+#    pg_dumpall -c -O > "$temporary_dir/db.backup"
+#    echo dump was created
+
+    echo Copying ngw dir - "$HOME/projects/ngw/" - to temporary_dir - "$temporary_dir/"
+    mkdir -p "$temporary_dir/ngw"
+    rsync -a -L --exclude=".*" --exclude ".*/" "$HOME/projects/ngw/" "$temporary_dir/ngw"
+    echo OK
+
+    echo Copying widgets dir - "$HOME/projects/widgets/" - to temporary_dir - "$temporary_dir/"
+    mkdir -p "$temporary_dir/widgets"
+    rsync -a -L --exclude=".*" --exclude ".*/" "$HOME/projects/widgets/" "$temporary_dir/widgets"
+    echo OK
+
+    echo Creating archive "$zip_file_name"
+    tar -zhcf "$zip_file_name" -C "$temporary_dir" .
+    echo OK
+
+    echo Copying archive to backup_dir - "$backup_dir"
+    cp "$zip_file_name" "$backup_dir"
+    echo OK
+
+    echo Removing temporary_dir
+    rm -r "$temporary_dir"
+    echo OK
+
+    echo Removing temporary archive
+    rm "$zip_file_name"
+    echo OK
+
+    echo Dump was finished successfully.
 }
 
 backup $1 $2
